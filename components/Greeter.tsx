93a29b817b7dc484d305df1c0a16d24789d94d3f
Kristian Freeman <kristian@kristianfreeman.com>
2018-10-07 16:00:25 -0500

    Example page and component

---
+++ components/Greeter.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+type Props = {
+  text: string;
+};
+
+export default ({ text }: Props) => <h1>{text}</h1>;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+import Greeter from "../components/Greeter";
+
+export default () => (
+  <div>
+    <Greeter text="Hello, world!" />
+  </div>
+);
